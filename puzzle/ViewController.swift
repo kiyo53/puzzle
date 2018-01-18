70285491bf0088c5e6b29b8d56849ba505f0a40b
Shimizu Ayumi <ayutan20010503@docomo.ne.jp>
2018-01-18 18:37:14 +0900

    Initial Commit

---
+++ puzzle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  puzzle
+//
+//  Created by Ayumi Shimizu on 2018/01/18.
+//  Copyright © 2018年 Ayumi Shimizu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
